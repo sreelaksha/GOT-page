320139302041bf6bce6cd52b354e83edb5a4e89a
sreelaksha <sreelaksha.s@gmail.com>
2021-12-22 13:09:50 +0100

    - added styled-components package - added basic styling for HouseComponent

--- src/components/HouseComponent.js
+++ src/components/HouseComponent.js
@@ -1,5 +1,23 @@
 import React from "react"
 import HouseService from "../services/HouseService"
+import styled from 'styled-components'
+
+const OverviewWrapper= styled.div`
+
+`
+const HouseCardContainer = styled.section`
+        display: grid;
+        grid-template-rows: auto;
+        grid-gap: 20px;
+`
+
+const HouseCard= styled.div`
+        border: 1px solid rgba(0, 0, 20, 0.1);
+        border-radius: 10px;
+        box-shadow: 0 2px 4px rgba(0, 0, 0, 0.04);
+        padding: 10px;
+        margin: 10px 0;
+`
 
 export default class HouseComponent extends React.Component {
     constructor(){
@@ -18,13 +36,18 @@ export default class HouseComponent extends React.Component {
     }
     render(){
         return(
-            <div>
-                <h2>Game Of Thrones Overview</h2>
-                {this.state.houses.map(
-                    (house) => 
-                        <p> {house.name} </p>
-                )}    
-            </div>
+            <OverviewWrapper>
+                <h1>Game Of Thrones Overview</h1>
+                <HouseCardContainer>
+                    {this.state.houses.map(
+                        (house, index) => 
+                        <HouseCard key={index}>
+                            <p> {house.name} </p>
+                            <p>{house.region}</p>
+                        </HouseCard>
+                    )}  
+                </HouseCardContainer>  
+            </OverviewWrapper>
         )
     }
 }
