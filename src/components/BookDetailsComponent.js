243c336ff037daa7dda288b3ede156c0fc29f8fe
sreelaksha <sreelaksha.s@gmail.com>
2021-12-24 13:04:14 +0100

    - Fixed styling in book details

--- src/components/BookDetailsComponent.js
+++ src/components/BookDetailsComponent.js
@@ -2,7 +2,7 @@ import React from 'react'
 import BookService from '../services/BookService'
 import { withRouter } from "react-router-dom";
 import AttributeValueComponent from './shared/AttributeValueComponent'
-import {Card} from '../styles/CardComponent'
+import {WideCard} from '../styles/CardComponent'
 import Page from "./shared/Page"
 
 class BookDetailsComponent extends React.Component {
@@ -30,7 +30,7 @@ class BookDetailsComponent extends React.Component {
                     <h1>Book</h1>
                     {this.state.books && (
                     <section>
-                            <Card>
+                            <WideCard>
                                 <AttributeValueComponent label="Name" value={this.state.books.name} />
                                 <AttributeValueComponent label="ISBN" value={this.state.books.isbn} /> 
                                 <AttributeValueComponent label="Authors" value={this.state.books.authors} />
@@ -41,7 +41,7 @@ class BookDetailsComponent extends React.Component {
                                 <AttributeValueComponent label="Released" value={this.state.books.released} /> 
                                 <AttributeValueComponent label="Characters" value={this.state.books.characters} />
                                 <AttributeValueComponent label="Pov Characters" value={this.state.books.povCharacters} /> 
-                            </Card>
+                            </WideCard>
                     </section>
                 )}
             </Page>
